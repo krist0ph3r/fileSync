8bdc554bc04c2b068d03aca38baab18db541c97f
* <kristopher.n@gmail.com>
2018-03-11 11:57:10 +0530

    runs with full recursion

--- file_compare.py
+++ file_compare.py
@@ -2,25 +2,26 @@
 import sys
 import os
 
-rootDir = sys.argv[1]
-allfiles = {}
-
 def addfile(file):
     if file[1] in allfiles:
         allfiles[file[1]].append(file[0])
     else:
         allfiles[file[1]] = [file[0]]
 
-for dirName, subdirList, fileList in os.walk(rootDir):
-    #print('Found directory: %s' % dirName)
-    for fname in fileList:
-        fullpath = '%s/%s' % (dirName , fname)
-        size = os.path.getsize(fullpath)
-        addfile((fullpath,size))
-    # Remove the first entry in the list of sub-directories
-    # if there are any sub-directories present
-    if len(subdirList) > 0:
-        del subdirList[0]
+def scanfiles(dir):
+    for dirName, subdirList, fileList in os.walk(dir):
+        for fname in fileList:
+            fullpath = '%s/%s' % (dirName , fname)
+            size = os.path.getsize(fullpath)
+            addfile((fullpath,size))
+        #if len(subdirList) > 0:
+            #del subdirList[0]
+            #for subdir in subdirList:
+            #    print('%s/%s' % (dirName , subdir))
+
+rootDir = sys.argv[1]
+allfiles = {}
+scanfiles(rootDir)
 
 for file in allfiles:
     print(file,allfiles[file])
